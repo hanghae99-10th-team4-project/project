8f8fa987c368a9cb36312c33568ac2189c1f7f28
misracis2 <misracis@gmail.com>
2022-11-15 13:33:49 +0900

    save comment 업데이트

--- app_lsy.py
+++ app_lsy.py
@@ -11,26 +11,33 @@ db = client.sparta
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/index_lsy')
+def home_lsy():
     return render_template('index_lsy.html')
 
 @app.route("/api/walk", methods=["GET"]) #walk doc 가져오기
 def review_get():
-    review_give = db.walk.find_one({'post_id': ''})
+    review_give = list(db.walk.find({},{'_id':False}))
+    comment_give = list(db.walk_comment.find({},{'_id':False}))
 
-    return jsonify({'walk': review_give})
+    return jsonify({'review': review_give}, {'comment': comment_give})
 
 @app.route("/api/walk/comment", methods=["POST"]) #코멘트 저장하기
 def comment_post():
     post_id_receive = request.form['post_id']
     user_id_receive = request.form['user_id']
-    comment_id_receive = request.form['comment_id']
     comment_receive = request.form['comment_give']
     star_receive = request.form['star']
+
+    comment_list = list(db.walk_comment.find({},{'_id': False}))
+    count = len(comment_list) + 1
 
     doc = {
         'post_id' :post_id_receive,
         'user_id' : user_id_receive,
-        'comment_id' : comment_id_receive,
+        'comment_id' : count,
         'comment' : comment_receive,
         'star': star_receive
     }
